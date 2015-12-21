cb55b54f624b8c7de12c12b870d32207d280397a
Dmitri Skjorshammer <dmitriskj@gmail.com>
2015-12-20 19:54:21 -0800

    Adds API doc to BallotController.

--- app/controllers/api/v0/ballot_controller.rb
+++ app/controllers/api/v0/ballot_controller.rb
@@ -1,8 +1,35 @@
 class API::V0::BallotController < ActionController::API
+  resource_description do
+    api_version "v0"
+    formats ["json"]
+    param :ballot_uuid, String, :desc => "UUID of the ballot", :required => true
+    error 404, "Ballot does not exist"
+  end
 
+
+  #----------------------------------------------------------------------------
+  # GET /api/v0/ballot/:ballot_uuid/registration
+
+  api! "Retrieves the VotingBallot registration form along with its fields."
   def registration_form
   end
 
+  #----------------------------------------------------------------------------
+  # GET /api/v0/ballot/:ballot_uuid/registration
+
+  api! %Q(Verifies the values provided by the user for each field in the
+  VotingBallot registration form and responds with both the password for the
+  VotingBallot and a specific generated "signature" for the voter. The
+  signature will be used by the voter to retrieve their personal vote.)
+  example <<-EOS
+    JSON: {
+      "password": "...",
+      "signature": "..."
+    }
+  EOS
   def registration
   end
+
+  #----------------------------------------------------------------------------
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   apipie
   namespace :api, :defaults => {:format => "json"} do
     namespace :v0 do
-      resources :ballot, :only => [:create] do
+      resources :ballot, :only => [:create], :param => :uuid do
         get  "registration", :to => "ballot#registration_form"
         post "registration"
 
