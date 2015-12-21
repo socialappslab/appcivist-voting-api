1b91475a4de6edea3158e9da7065e9e8386504be
Dmitri Skjorshammer <dmitriskj@gmail.com>
2015-12-20 17:15:28 -0800

    Adds create/update/show routes to vote resource nested under ballot resource.

---
+++ app/controllers/api/v0/vote_controller.rb
@@ -0,0 +1,11 @@
+class API::V0::VoteController < ActionController::API
+
+  def create
+  end
+
+  def show
+  end
+
+  def update
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
       resources :ballot, :only => [] do
         get  "registration"
         post "registration"
+
+        resources :vote, :only => [:create, :show, :update]
       end
     end
   end
