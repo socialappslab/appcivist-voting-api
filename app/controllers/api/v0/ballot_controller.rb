ae2f5984431c498b70f40ebb0a83bef00c2af43a
Dmitri Skjorshammer <dmitriskj@gmail.com>
2015-12-20 22:03:08 -0800

    Adds API doc to VoteController.

--- app/controllers/api/v0/ballot_controller.rb
+++ app/controllers/api/v0/ballot_controller.rb
@@ -1,28 +1,36 @@
-class API::V0::BallotController < ActionController::API
+class API::V0::BallotController < ApplicationController
   resource_description do
     api_version "v0"
     formats ["json"]
-    param :ballot_uuid, String, :desc => "UUID of the ballot", :required => true
-    error 404, "Ballot does not exist"
   end
 
+  #----------------------------------------------------------------------------
+  # POST /api/v0/ballot
+
+  api! "Create the ballot"
+  def create
+  end
 
   #----------------------------------------------------------------------------
   # GET /api/v0/ballot/:ballot_uuid/registration
 
   api! "Retrieves the VotingBallot registration form along with its fields."
+  param_group :ballot, ApplicationController
+  error 404, "Ballot does not exist"
   def registration_form
   end
 
   #----------------------------------------------------------------------------
-  # GET /api/v0/ballot/:ballot_uuid/registration
+  # POST /api/v0/ballot/:ballot_uuid/registration
 
   api! %Q(Verifies the values provided by the user for each field in the
   VotingBallot registration form and responds with both the password for the
   VotingBallot and a specific generated "signature" for the voter. The
   signature will be used by the voter to retrieve their personal vote.)
+  param_group :ballot, ApplicationController
+  error 404, "Ballot does not exist"
   example <<-EOS
-    JSON: {
+    "vote": {
       "password": "...",
       "signature": "..."
     }
