70db67a7e44791f17a9b4d0423eedc4445c55fc0
Dmitri Skjorshammer <dmitriskj@gmail.com>
2015-12-29 10:35:52 -0800

    Add BallotPaper table and model to represent a user's ballot.

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,30 +1,23 @@
 class Vote < ActiveRecord::Base
-  module Status
-    DRAFT    = "DRAFT"
-    FINISHED = "FINISHED"
-  end
-
   #----------------------------------------------------------------------------
   # Validations
   #------------
-  validates :candidate_id, :presence => true
-  validates :ballot_id,    :presence => true
-  validates :signature,    :presence => true
-  validates :status,       :presence => true
-  validates :value,        :presence => true, :on => :update
-  validates :value_type,   :presence => true, :on => :update
+  validates :candidate_id,    :presence => true
+  validates :ballot_paper_id, :presence => true
+  validates :value,           :presence => true, :on => :update
+  validates :value_type,      :presence => true, :on => :update
 
   #----------------------------------------------------------------------------
   # Associations
   #-------------
   belongs_to :candidate
-  belongs_to :ballot
+  belongs_to :ballot_paper
 
   #----------------------------------------------------------------------------
 
   protected
 
   def self.permitted_params
-    [:signature, :value]
+    [:value]
   end
 end
